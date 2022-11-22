63552b5b3c5802231b19736b19be56c06d3cd7b0
Sergio <sergio977@rambler.ru>
2022-11-22 11:26:47 +0500

    add Ex_1 NEW3

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -35,14 +35,19 @@ int a1 = 15;
 int b1 = 4560;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 7;
 int c2 = 33;
 int a3 = 2250;
 int b3 = 23;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+int max = Max(
+    Max(a1 , b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+);
 Console.WriteLine(max);
