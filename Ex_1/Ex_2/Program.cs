b527a24b21b9a7eb7a63cc00e19809d09f4c7cb9
Sergio <sergio977@rambler.ru>
2022-11-22 11:37:54 +0500

    add ex_2 new2

--- Ex_1/Ex_2/Program.cs
+++ Ex_1/Ex_2/Program.cs
@@ -5,7 +5,7 @@
     if (arg3 > result) result = arg3;
     return result;
 }
-
+//             0   1   2   3   4   5   6   7   8
 int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
 array[0] = 12;
 Console.WriteLine(array[4]);
